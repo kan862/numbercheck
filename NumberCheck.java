6c9d1d648f1e2551cf5b0745ba7f45e298358865
kan862 <kanchannaregal@gmail.com>
2025-09-01 09:51:16 +0530

    Update NumberCheck.java

--- NumberCheck.java
+++ NumberCheck.java
@@ -2,7 +2,7 @@ public class NumberCheck {
     public static void main(String[] args) {
         int num=5;
         if(num > 0){
-            System.err.println("Number is positive");
+            System.out.println("Number is positive");
         }
     }
     
