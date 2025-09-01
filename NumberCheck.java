2fa26fbf494a52c22cfe7fe00dd9549515cc9f13
Naveen2138 <naveenkumarvajrad04@gmail.com>
2025-09-01 09:46:48 +0530

    save

---
+++ NumberCheck.java
@@ -0,0 +1,9 @@
+public class NumberCheck {
+    public static void main(String[] args) {
+        int num=5;
+        if(num > 0){
+            System.err.println("Number is positive");
+        }
+    }
+    
+}
